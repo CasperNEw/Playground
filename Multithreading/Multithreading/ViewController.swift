94907fb538ed710dc561aa55b97be5263c08ccc7
CasperNEW <CasperNEW@inbox.ru>
2020-03-13 11:48:56 +0300

    initial commit

---
+++ Multithreading/Multithreading/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Multithreading
+//
+//  Created by Дмитрий Константинов on 13.03.2020.
+//  Copyright © 2020 Дмитрий Константинов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
